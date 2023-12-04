cad5c95023a41cb9233fd88d88f0d6cc6bceb618
TyoKuncoro <tyodignitas@gmail.com>
2023-12-04 21:42:41 +0700

    feat: upload foto kitchen studio

--- src/kitchen_studio/kitchen_studio.controller.ts
+++ src/kitchen_studio/kitchen_studio.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, HttpStatus, Param, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, ParseUUIDPipe, UploadedFile, UseInterceptors, Post } from '@nestjs/common';
 import { KitchenStudioService } from './kitchen_studio.service';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storageProfile } from './helpers/upload_profile';
+import { MulterField } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 
 @Controller('kitchen-studio')
 export class KitchenStudioController {
     constructor(
         private kitchenStudioService: KitchenStudioService,
     ){}
+
+
+
+    @Post('/upload')
+        @UseInterceptors(FileInterceptor('file', storageProfile))
+        uploadFile(@UploadedFile() file: Express.Multer.File) {
+            if (typeof file?.filename == "undefined"){
+                return {
+                    statusCode: HttpStatus.BAD_REQUEST,
+                    message: "error file cannot be uploaded"
+                }
+            } else {
+                return {fileName: file?.filename}
+            }
+        }
 
 
     @Get('/all')
